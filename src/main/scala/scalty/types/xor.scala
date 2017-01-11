525def747835bf1a26e385faf717be887e19489e
Nazar Kisil <nazarkisil91.nk@gmail.com>
2017-01-11 12:01:51 +0200

    foldable for XorType

--- src/main/scala/scalty/types/xor.scala
+++ src/main/scala/scalty/types/xor.scala
@@ -1,5 +1,6 @@
 package scalty.types
 
+import cats._
 import cats.data.Xor
 import scalty.types.XorExtensions.XorMatcherExtension
 
@@ -38,10 +39,46 @@ object XorExtensions {
 
   }
 
+  final class FoldableExtension[T](val values: List[XorType[T]]) {
+    def foldable: XorType[List[T]] =
+      Foldable[List].foldMap(values)(a => a.map(List(_)))(xor.xorTypeMonoid[T])
+
+    def foldableSkipLeft: XorType[List[T]] =
+      Foldable[List].foldMap(values)(a => a.map(List(_)))(xor.xorTypeIgnoreLeftMonoid[T])
+
+    def foldableMap[D](f: (T) => D): XorType[List[D]] =
+      Foldable[List].foldMap(values)(a => a.map(value => List(f(value))))(xor.xorTypeMonoid[D])
+  }
+
 }
 
 case class XorMatcherException(msg: String) extends Exception(msg)
 
 object xor extends XorTypeAlias {
   val EMPTY_XOR = Xor.right[AppError, Empty](empty.EMPTY_INSTANCE)
+
+  implicit def xorTypeMonoid[T]: Monoid[XorType[List[T]]] = new Monoid[XorType[List[T]]] {
+    override def empty: XorType[List[T]] = Xor.right[AppError, List[T]](List.empty[T])
+
+    override def combine(xOr: XorType[List[T]], yOr: XorType[List[T]]): XorType[List[T]] =
+      for {
+        x <- xOr
+        y <- yOr
+      } yield x ++ y
+  }
+
+  implicit def xorTypeIgnoreLeftMonoid[T]: Monoid[XorType[List[T]]] = new Monoid[XorType[List[T]]] {
+    override def empty: XorType[List[T]] = Xor.right[AppError, List[T]](List.empty[T])
+
+    override def combine(xOr: XorType[List[T]], yOr: XorType[List[T]]): XorType[List[T]] =
+      for {
+        x <- xOr.recover {
+          case anyError => List.empty[T]
+        }
+        y <- yOr.recover {
+          case anyError => List.empty[T]
+        }
+      } yield x ++ y
+  }
+
 }
