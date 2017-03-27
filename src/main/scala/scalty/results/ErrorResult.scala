5d5fb738ed3bcf70d6a34443508813634e42b43c
Nazar Kisil <nazarkisil91.nk@gmail.com>
2017-03-27 15:16:51 +0300

    full stacktrace for ExceprionResult

--- src/main/scala/scalty/results/ErrorResult.scala
+++ src/main/scala/scalty/results/ErrorResult.scala
@@ -1,8 +1,18 @@
 package scalty.results
 
+import java.io.{PrintWriter, StringWriter}
+
 /**
   * Created by kisilnazar on 01.10.16.
   */
 trait ErrorResult extends Result
 
-case class ExceptionResult(throwable: Throwable) extends ErrorResult
+case class ExceptionResult(throwable: Throwable) extends ErrorResult {
+
+  override def toString: String = {
+    val writer = new StringWriter()
+    throwable.printStackTrace(new PrintWriter(writer))
+    s"ExceptionResult(${writer.toString})"
+  }
+
+}
